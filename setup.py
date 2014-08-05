f53685b350648c90034b6156988f2ed4c6d9ce3e
Kang HyoJun <admire9@gmail.com>
2014-08-05 18:20:30 +0900

    Add structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(name='tento',
+      version='0.0.1',
+      author='Kang Hyojun',
+      author_email='admire9@gmail.com',
+      packages=find_packages(),
+      install_requires=[
+          'flask==0.10.1', 'flask-script==2.0.5', 'sqlalchemy==0.9.7',
+          'pytest==2.6.0', 'tox', 'alembic'
+      ],
+      scripts=['manager.py'])
