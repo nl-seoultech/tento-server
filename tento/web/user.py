c7bf861a52972c797710031dcc2a6024079e929a
Kang Hyojun <admire9@gmail.com>
2014-08-06 09:38:59 -0700

    회원가입 작동하도록
    close #2

--- tento/web/user.py
+++ tento/web/user.py
@@ -1,22 +1,29 @@
+# -*- coding: utf-8 -*-
 from flask import Blueprint, jsonify, request, abort
-from tento.user import User
+from sqlalchemy.exc import IntegrityError
+
 from tento.db import session
-from sqlalchemy.exc import IntegrityError, UnboundExecutionError
+from tento.user import User
 
 bp = Blueprint('user', __name__)
 
 
 @bp.route('/', methods=['POST'])
 def create_user():
-    email = request.form.get('email', None) 
-    password = request.form.get('password', None)
+    if request.content_type == 'application/x-www-form-urlencoded':
+        email = request.form.get('email', None)
+        password = request.form.get('password', None)
+    elif request.content_type == 'application/json':
+        payload = request.json
+        email = payload.get('email', None)
+        password = payload.get('password', None)
     user = User(email=email, password=password)
     session.add(user)
     if email is None or password is None:
-        return jsonify(message='email, password must required')
+        abort(400)
     try:
         session.commit()
     except IntegrityError as e:
         session.rollback()
-        abort(400)
-    return jsonify(email=user.email)
+        abort(500)
+    return jsonify(email=user.email, id=user.id, name=user.name), 201
--- tests/web/test_web_user.py
+++ tests/web/test_web_user.py
@@ -1,32 +1,45 @@
 # -*- coding: utf-8 -*-
 from flask import json
 from ..util import url_for
+
 from tento.web.app import app
+from tento.user import User
+
+
+def _test_create_user(response, f_session):
+    mail = 'admire9@gmail.com'
+    assert 201 == response.status_code
+    assert response.data
+    data = json.loads(response.data)
+    assert 'id' in data
+    assert 'email' in data
+    assert 'name' in data
+    assert mail == data['email']
+    assert 'admire9' == data['name']
+    db_user = f_session.query(User)\
+              .all()
+    assert db_user
+    assert mail == db_user[0].email
 
 
 def test_web_create_user(f_session):
-    """ 유저를 생성하는 api를 테스트해봅니다.
-    테스트 코드는 아래와 비슷한 코드구조를 가지게됩니다.
-
-    .. sourcecode::python
-
+    """ 폼데이터로 유저생성
     """
-
+    mail = 'admire9@gmail.com'
     with app.test_client() as client:
         response = client.post(url_for('user.create_user'),
-                           data={'email': 'admire9@gmail.com',
-                                 'password': 'foobar'})
+                               data={'email': mail,
+                                     'password': 'foobar'})
+    _test_create_user(response, f_session)
 
 
-    assert 201 == response.status_code
-    assert response.data
-    json = json.loads(response.data)
-    assert 'id' in json
-    assert 'email' in json
-    assert 'name' in json
-    assert 'admire9@gmail.com' == json['email']
-    assert 'admire9' == json['name']
-    user = f_session.query(User)\
-              .all()
-    assert user
-    assert 'admire9@gmail.com' == db_user[0].mail
+def test_web_create_user_by_json(f_session):
+    """ json으로 유저생성
+    """
+    mail = 'admire9@gmail.com'
+    with app.test_client() as client:
+        response = client.post(url_for('user.create_user'),
+                               data=json.dumps({'email': mail,
+                                                'password': 'foobar'}),
+                               content_type='application/json')
+    _test_create_user(response, f_session)
