6d430a83ad8b8058dad582caf2d9ece789a70639
gong seong hyeon <tanesia_cou@naver.com>
2014-08-08 01:28:06 +0900

    update tento/web/add.py

--- tento/web/app.py
+++ tento/web/app.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 
-from . import user
+from . import user, login
 
 
 app =  Flask(__name__)
-app.register_blueprint(user.bp, url_prefix='/users/')
+app.register_blueprint(user.bp, url_prefix='/users')
+app.register_blueprint(login.bp, url_prefix='/login')
 
 
 if __name__ == '__main__':
