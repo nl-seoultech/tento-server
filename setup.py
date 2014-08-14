4d3388d805e6fa12d33bb8323fdbd12a9e05d03c
Kang Hyojun <admire9@gmail.com>
2014-08-14 03:43:55 -0700

    sphinx 추가

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='tento',
       packages=find_packages(),
       install_requires=[
           'flask==0.10.1', 'flask-script==2.0.5', 'sqlalchemy==0.9.7',
-          'pytest==2.6.0', 'tox', 'alembic', 'python-bcrypt', 'arrow',
-          'itsdangerous'
+          'pytest==2.6.0', 'alembic', 'py-bcrypt==0.4', 'arrow',
+          'itsdangerous', 'sphinx==1.2.2', 'sphinx_rtd_theme'
       ],
       scripts=['manager.py'])
